3ea25d2a01f1a7af6bcfbb1e9f6b76c808901852
jordan-pierre <jordan.j.pierre@gmail.com>
2021-12-24 13:02:56 -0500

    Cleaned up results.  Some refactor

--- scraper.py
+++ scraper.py
@@ -7,33 +7,45 @@ import time
 import pandas as pd
 import numpy as np
 import re
+from statistics import mean
+
+CHROME_DRIVER_LOC = '/home/jordan/Documents/page-price-scraper/chromedriver'
+
+def load_page(url, static_mode=False):
+    '''Read webpage.  Need to use selenium to wait on JavaScript objects'''
+    html_file = 'selenium_page.html'
+
+    if static_mode:
+        # -- Stale version
+        with open(html_file, 'r') as f:
+            s_page = f.read()
+    else:
+        # -- Live version
+        driver = webdriver.Chrome(CHROME_DRIVER_LOC)
+        driver.get(url)
+        time.sleep(5)
+        s_page = driver.page_source
+        with open(html_file, 'w') as f:
+            print(s_page, file=f)
+    
+    return BeautifulSoup(s_page, 'html.parser')
 
 
 def main():
-    sample_page = 'https://www.thespruce.com/best-garage-heaters-4176054' # 'https://www.thespruce.com/best-canister-vacuums-4171592'
+    # sample_page = 'https://www.thespruce.com/best-canister-vacuums-4171592'
+    sample_page = 'https://www.thespruce.com/best-headboards-4165760'
+    # sample_page = 'https://www.thespruce.com/best-garage-heaters-4176054'
 
-    html_file = 'selenium_page.html'
-    # -- Live version
-    driver = webdriver.Chrome('/home/jordan/Documents/page-price-scraper/chromedriver')
-    driver.get(sample_page)
+    soup = load_page(sample_page, static_mode=True)
 
-    time.sleep(5)
-    s_page = driver.page_source
-    with open(html_file, 'w') as f:
-        print(s_page, file=f)
-
-    # -- Stale version
-    # with open(html_file, 'r') as f:
-    #     s_page = f.read()
-
-    soup = BeautifulSoup(s_page, 'html.parser')
-
-    
     product_data = []
 
     # Get page title: class="heading__title"
     page_titles = soup.find("h1", class_="heading__title")
-    print(page_titles)
+    try:
+        page_titles = re.findall('>(.*)<\/', str(page_titles))[0]
+    except IndexError:
+        page_titles = page_titles
 
     # Get product retailer: data-retailer-type
     product_sections = soup.find_all("div", class_="comp sc-list-item list-sc-item__content mntl-block")
@@ -48,14 +60,23 @@ def main():
         # Get product title: class="product-record__heading--text"
         product_titles = section.find("span", class_="product-record__heading--text")
         print(f"product_titles = {product_titles}")
-
+        try:
+            product_titles = re.findall('>([^<]*)<\/', str(product_titles))[0].replace('\n', ' ').strip()
+        except IndexError:
+            product_titles = product_titles
+        
+        # Get product ratings
         product_ratings = section.find("span", class_="star-rating__label")
+        try:
+            product_ratings = re.findall('>(\d*[.,]?\d*)<\/', str(product_ratings))[0]
+        except IndexError:
+            product_ratings = product_ratings
         print(f"product_ratings = {product_ratings}")
 
         
         product_details = section.find_all("a", class_="button mntl-commerce-button mntl-text-link js-extended-commerce__button")
         
-        details_list = []
+        price_details_list = []
         for details in product_details:
             with open('details.txt', 'w') as f:
                 print(section, file=f)
@@ -74,18 +95,24 @@ def main():
                 retailer = re.findall('View On ([a-zA-Z0-9\'_.-]*)<', str(details))[0]
             except IndexError:
                 retailer = np.nan
-            details_list.append({'price': price, 'retailer': retailer})
+            price_details_list.append({'retailer': retailer, 'price': price})
 
+        print(f"product_prices = {price_details_list}")
 
-        # TODO: Extract price, link, and site
-        print(f"product_prices = {details_list}")
+        prices = [float(x['price'].strip('$').replace(',', '')) for x in price_details_list if isinstance(x['price'], str)]
+        avg_product_price = mean(prices) if len(prices) > 0 else np.nan
         
-        product_data.append([page_titles, product_titles, product_ratings, details_list])
+        product_data.append([page_titles, product_titles, product_ratings, price_details_list, avg_product_price])
     
 
-    header_row = ['page_titles', 'product_titles', 'product_ratings', 'product_prices']
+    header_row = ['page_titles', 'product_titles', 'product_ratings', 'product_prices', 'avg_product_price']
     df = pd.DataFrame(product_data, columns=header_row)
+
+    print(df.groupby('page_titles').mean())
+    
     df.to_csv('product_data.csv')
+
+    # TODO: Crawler to read all the links in the bottom section
 
     return
 
